65ffd45e102e17d02bcbed7d1549264a78a23134
Akira <Hicham.bourezi@gmail.com>
2023-08-13 05:22:05 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: always return the value 0
+ */
+
+int main(void)
+{
+char i;
+for (i = 'z'; i <= 'a'; i--)
+{
+putchar(i);
+}
+
+return (0);
+}
