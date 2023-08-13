861455c7cb852cede5ac2197444991b91ed1151f
Akira <Hicham.bourezi@gmail.com>
2023-08-13 02:43:44 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 {
 int i;
 char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-for (i = 0 ; i != '\0'; i++)
+for (i = 0 ; quote[i] != '\0'; i++)
 {
 putchar(quote[i]);
 }
