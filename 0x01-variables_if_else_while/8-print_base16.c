7a38d336a989969097beac5399bc8dcb5062601f
Akira <Hicham.bourezi@gmail.com>
2023-08-13 08:03:39 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - the min function
+ *
+ * Return: always return the value 0
+ */
+
+int main(void)
+{
+
+int i;
+int a;
+for (i = 0; i <= 9; i++)
+{
+putchar('0' + i);
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
+}
+putchar('\n';)
+return (0);
+}
