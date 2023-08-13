1171983cd6f2437a1a0add58a3421cdbe0c5c423
Akira <Hicham.bourezi@gmail.com>
2023-08-12 17:00:09 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ * Return: it always return the value 0
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
