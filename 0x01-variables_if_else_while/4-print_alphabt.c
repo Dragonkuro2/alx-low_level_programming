5acbf6ec088efe3b5499e250b795131f9fbdbb2b
Akira <Hicham.bourezi@gmail.com>
2023-08-13 04:33:22 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: it always return the value 0
+ *
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != q && i != e)
+{
+putchar(i);
+}
+}
+return (0);
+}
