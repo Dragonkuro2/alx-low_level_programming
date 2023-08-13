d0c0575e5f2b1cf8b2c9720d17e686ea7cda8a37
Akira <Hicham.bourezi@gmail.com>
2023-08-12 17:59:10 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: always return the value 0
+ *
+ */
+
+int main(void)
+{
+int i;
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+for (i = 0 ; i != '\0'; i++)
+{
+putchar(quote[i]);
+}
+
+return (0);
+}
