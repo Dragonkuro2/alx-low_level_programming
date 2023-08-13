2ec02c3d10fab045cd426628db2b3588a1db59b8
Akira <Hicham.bourezi@gmail.com>
2023-08-13 08:20:02 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ *Return: always return the value 0
+ *
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar('0' + i);
+putchar(',');
+putchar(' ');
+}
+
+return (0);
+}
