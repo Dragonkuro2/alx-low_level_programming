f53a66a9040dd6a12a67636a23a80dab05f50ef3
Akira <Hicham.bourezi@gmail.com>
2023-08-13 03:51:38 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: it always return the value 0
+ *
+ */
+
+int main(void)
+{
+char a;
+char A;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (b = 'A'; b <= 'Z'; b++)
+{
+putchar(b);
+}
+
+return (0);
+}
