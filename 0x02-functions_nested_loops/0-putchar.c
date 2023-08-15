5367f94b22e52833aa12814b2874d88b0201a4c1
Akira <Hicham.bourezi@gmail.com>
2023-08-15 03:46:15 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main fucntion
+ *
+ * Return: always return the value 0
+ *
+ */
+
+int main(void)
+{
+int i;
+char T[] = "_putchar";
+for (i = 0; i != '\0'; i++)
+	putchar(T(i));
+
+return (0);
+}
