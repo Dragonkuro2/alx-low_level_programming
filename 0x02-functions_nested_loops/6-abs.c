f3e63a0c4278cefddbeba8a748ea776a7b00bc60
Akira <Hicham.bourezi@gmail.com>
2023-08-17 12:32:21 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - the function that will turn any number to positive number
+ *
+ * Return: it return the positive value of r
+ *
+ * @r: the number that we want
+ */
+
+int _abs(int r)
+
+{
+if (r >= 0)
+	return (r);
+else
+	return (-r);
+}
