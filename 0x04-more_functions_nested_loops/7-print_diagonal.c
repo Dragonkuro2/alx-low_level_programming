c1b8bae52afaab229aa83b2093a46a3bcf14f1f4
Akira <Hicham.bourezi@gmail.com>
2023-08-19 12:40:27 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - the function that print the diagonals
+ *
+ * @n: number of \
+ */
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n <= 0)
+	_putchar('\n');
+
+else
+for (i = 0; i < n; i++)
+{
+	for (j = 0; j < i; j++)
+	_putchar(' ');
+	_putchar('\\');
+	_putchar('\n');
+
+}
+}
