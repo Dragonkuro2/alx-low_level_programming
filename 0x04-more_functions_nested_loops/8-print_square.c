e0b0c9e3846c4f7133913ec08e34ea9ddd8f1664
Akira <Hicham.bourezi@gmail.com>
2023-08-19 13:54:47 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - the function the print the square using #
+ *
+ * @size: the number that will be the square
+ */
+
+void print_square(int size)
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+
+}
