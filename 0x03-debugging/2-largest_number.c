d15d97fe6911d2bdce83944f72d0d890b4fa287b
Akira <Hicham.bourezi@gmail.com>
2023-08-18 18:09:33 -0700

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b > c || a > c >)
 	{
 	largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a > c || b > c > a)
 	{
 	largest = b;
 	}
-	else
+	else if (c > b > a || c > a > b)
 	{
 	largest = c;
 	}
