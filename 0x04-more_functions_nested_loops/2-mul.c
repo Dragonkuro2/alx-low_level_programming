bc4cc240e195456081a104ff2cfabc5ae3985e97
Akira <Hicham.bourezi@gmail.com>
2023-08-19 06:12:43 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - the function that multiple the two numbers
+ *
+ * @a: the first number
+ *
+ * @b: the second number
+ *
+ * Return: return the value of mul
+ */
+
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+
+return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - the function that write all the number from 0 to 9
+ *
+ * Return: always return the value 0
+ *
+ */
+
+void print_numbers(void)
+{
+
+int i;
+for (i = '0'; i <= '9'; i++)
+	_putchar(i);
+_putchar('\n');
+
+}
