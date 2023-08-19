ac5929d0e574bcd681ed9419283c33bf898e9fce
Akira <Hicham.bourezi@gmail.com>
2023-08-19 13:57:34 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,10 @@
 void print_square(int size)
 {
 int i, j;
+if (size <= 0)
+	_putchar('\n');
+else
+{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
@@ -17,5 +21,5 @@ _putchar('#');
 }
 _putchar('\n');
 }
-
+}
 }
