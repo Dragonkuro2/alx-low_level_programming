9cf3432864d7b9b0752476f55051d63efd4ea694
Akira <Hicham.bourezi@gmail.com>
2023-08-21 16:49:41 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - this function prints all the arrays with comma
+ *
+ * @a: the first pointer
+ *
+ * @n: the tall of the array
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+		if (i < n - 1)
+			printf(" ,");
+}
+printf("\n");
+}
