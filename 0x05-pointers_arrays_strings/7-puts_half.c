fc57a19b17c644fd66e0c58a3952be7d2f63dc71
Akira <Hicham.bourezi@gmail.com>
2023-08-21 15:54:13 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - the function that prints the end half of the text
+ *
+ * @str: this input
+ *
+ */
+
+void puts_half(char *str)
+{
+int i, j;
+for (i = 0; str[i] != '\0'; i++);
+for (j = i / 2; str[j] != '\0'; j++)
+	_putchar(str[j]);
+_putchar('\n');
+
+}
