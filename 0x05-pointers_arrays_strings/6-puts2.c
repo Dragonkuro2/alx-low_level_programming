15b21156ebe7a4d90e1400fe8209f687b0a619d6
Akira <Hicham.bourezi@gmail.com>
2023-08-21 13:44:07 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - this function write just the number that contain 2
+ *
+ * @str: this is the input number
+ *
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] % 2 == 0)
+	_putchar(str[i]);
+}
+_putchar('\n');
+}
