b7ee1f476e75da8209f2107afeacb847e3f06b80
Akira <Hicham.bourezi@gmail.com>
2023-08-21 15:56:26 -0700

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,8 @@
 void puts_half(char *str)
 {
 int i, j;
-for (i = 0; str[i] != '\0'; i++);
+for (i = 0; str[i] != '\0'; i++)
+	;
 for (j = i / 2; str[j] != '\0'; j++)
 	_putchar(str[j]);
 _putchar('\n');
