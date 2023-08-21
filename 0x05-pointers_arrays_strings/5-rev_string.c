9a4e1d0865bd3faea64c787f7f2991f4253a3432
Akira <Hicham.bourezi@gmail.com>
2023-08-21 12:50:50 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,16 @@
 void rev_string(char *s)
 {
 int i = 0;
-int j;
-while (*(s + i) != '\0')
+int tmp, j;
+while (s[i])
 	i++;
-for (j = 0; j <= i; j++)
-	_putchar(*(s + i - j));
-_putchar('\n');
 
+for (j = 0; j <= i / 2; j++)
+{
+tmp = s[i];
+s[i] = s[j];
+s[j] = tmp;
+i--;
+}
+_putchar('\n');
 }
