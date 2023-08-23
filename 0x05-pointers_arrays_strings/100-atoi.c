29ec792a0b9b8cc073edc1ba83ad17e053c969dd
Akira <Hicham.bourezi@gmail.com>
2023-08-22 17:56:21 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,23 +9,17 @@
   */
 int _atoi(char *s)
 {
-unsigned int i = 0, res = 0;
+unsigned int num = 0;
 int sign = 1;
-while (s[i])
-{
-	if (s[i] == '-')
-		sign *= -1;
-i++;
-}
-i = 0;
-while (s[i] != '\0')
-{
-	if (s[i] <= '9' && s[i] >= '0')
-	{
-		res = (res * 10) + (s[i] - '0');
-	}
-		i++;
-}
 
-return (res * sign);
+do {
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+num = num * 10 + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+
+return (num * sign);
 }
