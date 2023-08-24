c294378b94753605e1f600093b603994f968b4c1
Akira <Hicham.bourezi@gmail.com>
2023-08-24 14:54:17 -0700

    changes in task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,17 +11,23 @@
 char *rot13(char *s)
 {
 int i, j;
-char s1[] = "ABCDEFGHIJKLMabcdefghijklm";
-char s2[] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+char s1[] = "ABCDEFGHIJKLM";
+char s2[] = "NOPQRSTUVWXYZ";
+char s3[] = "abcdefghijklm";
+char s4[] = "nopqrstuvwxyz";
 
-for (i = 0 ; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 26; j++)
+for (j = 0; j < 13; j++)
 {
 if (s[i] == s1[j])
-	s[i] += 13;
+	s[i] = s2[j];
 else if (s[i] == s2[j])
-	s[i] -= 13;
+	s[i] = s1[j];
+else if (s[i] == s3[j])
+	s[i] = s4[j];
+else if (s[i] == s4[j])
+	s[i] = s3[j];
 }
 
 }
