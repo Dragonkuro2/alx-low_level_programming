55ab4c49ce9f7e9459359e1538afbb1fed5b0b6b
Akira <Hicham.bourezi@gmail.com>
2023-08-25 04:48:21 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,13 +13,42 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i;
+int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-*r = *n1 + *n2;
-for (i = 0; *(r + i) != '\0'; i++)
-	;
-if (size_r < i + 1)
-	return (0);
+while (n1[i] != '\0')
+i++;
+while (n2[j] != '\0')
+j++;
+if (i > j)
+l = i;
 else
-	return (r);
+l = j;
+if (l + 1 > size_r)
+return (0);
+r[l] = '\0';
+for (k = l - 1; k >= 0; k--)
+{
+i--;
+j--;
+if (i >= 0)
+f = n1[i] - 48;
+else
+f = 0;
+if (j >= 0)
+s = n2[j] - 48;
+else
+s = 0;
+r[k] = (f + s + d) % 10 + '0';
+d = (f + s + d) / 10;
+}
+if (d == 1)
+{
+r[l + 1] = '\0';
+if (l + 2 > size_r)
+return (0);
+while (l-- >= 0)
+r[l + 1] = r[l];
+r[0] = d + 48;
+}
+return (r);
 }
