459e559bc1663f8c420d9493828e82dc6cc3edd1
Akira <Hicham.bourezi@gmail.com>
2023-08-28 08:32:36 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - this function is the same strchr function
+ *
+ * @s: the input pointer
+ * @c: the carachter that we are looking for
+ *
+ * Return: itreturn NULL or the rest of the pointer s
+ */
+
+char *_strchr(char *s, char c)
+{
+size_t i;
+int witness = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+witness = i;
+break;
+}
+}
+if (witness != 0)
+	return (&s[witness]);
+else
+	return (NULL);
+}
