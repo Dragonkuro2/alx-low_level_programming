db6434cb066bde45c81a0e822b05f2cd0aa32622
Akira <Hicham.bourezi@gmail.com>
2023-08-31 06:22:14 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - this function and the function puts are the same
+ *
+ * @s: the input string
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+	_putchar('\n');
+else
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+In this folder you can find all the tasks of the project '0x08. C - Recursion'
