0c0bcc3e5bfda81c2e46de110aeb33f426f52bda
Akira <Hicham.bourezi@gmail.com>
2023-09-03 05:59:07 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - the entry point
+ *
+ * @argc: the counter
+ * @argv: the pointer of the array
+ *
+ * Return: it always return the value 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+In this filder you can find all the tasks of the project 0x0A-argc_argv
