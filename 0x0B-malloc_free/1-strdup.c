a409bae7d9972eb3cb0df429ea5667bb8400b686
Akira <Hicham.bourezi@gmail.com>
2023-09-04 09:17:42 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - this function is just like the function strdup
+ *
+ * @str: the array that we want to copy
+ *
+ * Return: it depends on output of the function
+ */
+
+char *_strdup(char *str)
+{
+size_t i;
+char *s = (char *)malloc(sizeof(char) * strlen(str) + 1);
+
+if (str == NULL || s == NULL)
+	return (NULL);
+else
+	for (i = 0; i <= strlen(str); i++)
+		*(s + i) = *(str + i);
+return (s);
+
+}
