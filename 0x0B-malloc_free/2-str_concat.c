b051850792e3c31faefdd521e8c2904bc59979d7
Akira <Hicham.bourezi@gmail.com>
2023-09-04 11:28:09 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * str_concat - this function prints s1 + s2
+ *
+ * @s1: the first text
+ * @s2: the second text
+ *
+ * Return: depands on the output of the function
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+size_t i, j = 0;
+
+char *s3 = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 2));
+if (s3 == NULL)
+        return (NULL);
+for (i = 0; i < strlen(s1); i++)
+*(s3 + i) = *(s1 + i);
+s3[i] = ' ';
+for (; i < (strlen(s1) + strlen(s2) + 2); i++, j++)
+	*(s3 + i) = *(s2 + j);
+return (s3);
+}
