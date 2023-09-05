50ad83ae94e93e071ac4450b7cf1beaff99bc5aa
Akira <Hicham.bourezi@gmail.com>
2023-09-05 15:53:28 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,15 +2,16 @@
 #include <string.h>
 
 /**
+ * strtow - this function is like the function strtow
  *
+ * @str: the input pointer
  *
- *
- *
- *
+ * Return: it depands on the output of the function
  */
 
 char **strtow(char *str)
 {
-
+if (str == NULL || str == "")
+	return (NULL);
 
 }
