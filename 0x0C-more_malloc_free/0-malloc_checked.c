b6411af48261639e3c817f26cd9d75c89fcbe984
Akira <Hicham.bourezi@gmail.com>
2023-09-07 06:25:42 -0700

    I just changed the place of the files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - this function just check if we can use the function malloc or no
+ *
+ * @b: the number of biyts
+ *
+ * Return: it return the allocated memmory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+if (ptr == NULL)
+	exit (98);
+else
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+In this folder you'll find all the task that I did from the project 0x0C-more_malloc_free
