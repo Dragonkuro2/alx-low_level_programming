80beb64d85c493e411ad9a362df8cffa61f54364
Akira <Hicham.bourezi@gmail.com>
2023-09-07 06:38:08 -0700

    fixed code

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked - this function just check if we can use the function malloc or no
+ * malloc_checked - this function just check
+ *		if we can use the function malloc or no
  *
  * @b: the number of biyts
  *
@@ -12,7 +13,7 @@ void *malloc_checked(unsigned int b)
 {
 void *ptr = malloc(b);
 if (ptr == NULL)
-	exit (98);
+	exit(98);
 else
 	return (ptr);
 }
