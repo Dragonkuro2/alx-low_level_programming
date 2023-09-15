0071c4ccfde94b8168173126b5eb56dde9f4f6e4
Akira <Hicham.bourezi@gmail.com>
2023-09-14 17:37:20 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all - this function prints all the type of arg
+ * @format: the format of the arg
+ * @...: the args
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list list;
+va_start(list, format);
+return;
+}
