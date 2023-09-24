a1ca343cfc0413ba727f1ed0107b32acedd07108
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-24 12:07:00 -0700

    I fixed the error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,13 @@
 
 size_t print_list(const list_t *h)
 {
-int i = 0;
+size_t i = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%ld] %s\n", strlen(h->str), h->str);
 		i++;
 		h = h->next;
 	}
