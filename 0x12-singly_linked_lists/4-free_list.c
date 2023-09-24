f83ad59631c3a0fe610d9479924a572c02e3e729
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-24 11:29:25 -0700

    the function of the task number 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - this function free the memory for the input list
+ * @head: the input list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+	while (head != NULL)
+	{
+		current =head;
+		head = head->next;
+		if (current->str != NULL)
+			free(current->str);
+		free(current);
+	}
+}
