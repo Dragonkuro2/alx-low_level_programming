8fcdc51f93e4f8f3429c7058e5205bbc7b729708
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-24 16:01:35 -0700

    the function of the task number 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+/**
+ * _constructor - this function executes before main()
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
