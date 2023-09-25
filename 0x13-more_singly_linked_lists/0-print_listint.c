a7acb72bea719ba69582fae496dcc0661445f118
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-25 09:14:31 -0700

    i made just a small changes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 int i = 0;
 	if (h == NULL)
 		return (0);
-while (h)
-{
-	i++;
-	printf("%d\n", h->n);
-	h = h->next;
-}
-return (i);
+	while (h != NULL)
+	{
+		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
