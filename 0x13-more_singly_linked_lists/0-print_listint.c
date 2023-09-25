d42f7395ce8e75477175d97dc548358fb4513330
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-25 04:45:45 -0700

    the function of the task number 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - this function prints all the nodes of the list
+ * @h: the input list
+ * Return: it return the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+int i = 0;
+	if (h == NULL)
+		return (0);
+while (h)
+{
+	i++;
+	printf("%d\n", h->n);
+	h = h->next;
+}
+return (i);
+}
