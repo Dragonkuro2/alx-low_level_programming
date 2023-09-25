959f00f424c8a863063244fbbe63d6038831db5b
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-25 09:36:51 -0700

    the function of the task number 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - this function free the inout list
+ * @head: the input list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+	if (*head == NULL)
+		return;
+	while (*head)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+
+}
