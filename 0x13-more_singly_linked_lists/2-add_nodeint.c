1507649b669a66f965bb8fbf490bc33c5c85919c
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-25 05:25:41 -0700

    the function of the task number 2 that added a node at the beginning of the list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/*
+ * add_nodeint - this function add a node with type int
+ * @head: the input head
+ * @n: the data
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *node;
+node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = *head;
+	*head = node;
+	return (node);
+}
