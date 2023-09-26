28d73ee55365ecf05c438e0b033202d6cd861803
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-26 07:27:24 -0700

    the function of the task number 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
  * int_index - this function that searches for an integer
  * @array: the inout array
  * @size: the size
- * @cmp the inout function
+ * @cmp: the inout function
  * Return: 0, 1 or -1 it depands on the result
  */
 
