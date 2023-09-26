ce9baf52284d720505a89569f82fd38368d6250b
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-26 07:25:57 -0700

    the function of the task number 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,11 +13,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 
 	if (array && size && cmp)
-		while (i > size)
+		while (i < size)
 		{
 			if (cmp(array[i]))
 				return (i);
-			i++
+			i++;
 		}
 	return (-1);
 }
