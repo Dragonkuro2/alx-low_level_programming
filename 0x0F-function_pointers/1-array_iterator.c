30045e012b9c119cb07ad7ee1cac969d4727bfbb
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-26 06:39:49 -0700

    the function of the task number 8

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int *last = array + size - 1;
+int *last = array + size;
 
 if (array && size && action)
 	while (array < last)
