17c830082b39006684f8f365413b7791d2c655dd
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-26 17:47:30 -0700

    the first version of the function 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - this function prints a listint_t linked list
+ * @head: the input list
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+if (head == NULL)
+	exit(98);
+return (98);
+}
