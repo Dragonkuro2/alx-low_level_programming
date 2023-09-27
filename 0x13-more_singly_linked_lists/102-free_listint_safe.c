63d992656a02c1aca2ac3fa3cffeeaf380783934
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-26 17:54:10 -0700

    the first version of the task number 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - this function frees a listint_t list
+ * @h: the input list
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	if (*h == NULL)
+		return (0);
+return (0);
+}
