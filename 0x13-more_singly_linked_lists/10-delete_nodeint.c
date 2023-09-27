ffb84221aa81dd7f88219cc1bf19ea472c647184
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-26 17:03:48 -0700

    I fixed the error I guess

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *node, *node2;
 size_t i = 0;
-node = *head, node2 = *head;
+node = *head;
 	if (head == NULL)
 		return (-1);
 	if (index == 0)
