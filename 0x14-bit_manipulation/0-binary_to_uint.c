d7d9b9fc52a7592d159b3ecfd5365d07781ff7f3
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-28 10:08:53 -0700

    the function of the task number 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - this function converts a binary number to an unsigned int
+ * @b: the input binary number
+ * Return: the converted number, or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	size_t i = 0, num = 0;
+	if (b == NULL)
+		return (0);
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		else
+			num += (b[i] - '0') * pow(2,strlen(b) - i - 1);
+	}
+	return (num);
+
+}
