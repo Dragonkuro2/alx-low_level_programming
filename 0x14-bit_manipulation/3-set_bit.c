bab810c665e75d7d05b714d85af47362e938af5a
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-29 15:08:46 -0700

    the function of the task number 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit - this function set anumber at the certain index
+ * @n: the input number
+ * @index: the index
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || index >= (sizeof(n) * 8))
+		return (-1);
+	return (*n |= 1LU << index);
+}
