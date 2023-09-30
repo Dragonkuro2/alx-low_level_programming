21ed0635ef3529ee43fc67cdb14717fec56090ec
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-29 18:01:34 -0700

    I added the prototype of the advanced task

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
@@ -13,5 +12,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif /*MAIN_H*/
