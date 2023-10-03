b4d6d919ab741d785085896bd0f7c1ee62ed1cf3
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-10-03 06:15:12 -0700

    I used the loop while

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int checker;
+	int checker, counter = 0;
 	ssize_t checker2;
 
 	if (!filename || !text_content)
@@ -17,7 +17,11 @@ int create_file(const char *filename, char *text_content)
 	checker = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (checker == -1)
 		return (-1);
-	checker2 = write(checker, text_content, strlen(text_content));
+	while (text_content[counter])
+	{
+		counter++;
+		checker2 = write(checker, text_content, counter);
+	}
 	if (checker2 == -1)
 		return (-1);
 	close(checker);
