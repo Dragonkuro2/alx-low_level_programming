b51c7d86b2938c0f1002d766cb88c41d99bc47d5
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-10-03 06:06:43 -0700

    I fixed the error of betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename || !text_content)
 		return (-1);
-	checker = open(filename, O_CREAT| O_WRONLY | O_TRUNC , 0600);
+	checker = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (checker == -1)
 		return (-1);
 	checker2 = write(checker, text_content, strlen(text_content));
