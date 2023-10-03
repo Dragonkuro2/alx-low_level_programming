3f098207636c05cd33a229fcf2c853e8bf2c875c
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-10-03 04:43:42 -0700

    the function of the task number 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL || letters == 0)
 		return (0);
 	function = open(filename, O_RDONLY);
-	if (function < 0)
+	if (function == -1)
 		return (0);
 	array = malloc(letters);
 	if (array == NULL)
@@ -26,14 +26,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	checker1 = read(function, array, letters);
-	if (checker1 < 0)
+	if (checker1 == -1)
 	{
 		free(array);
 		close(function);
 		return (0);
 	}
 	checker2 = write(STDOUT_FILENO, array, letters);
-	if (checker2 < 0)
+	if (checker2 == -1)
 	{
 		free(array);
 		close(function);
