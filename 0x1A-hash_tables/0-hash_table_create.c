d02632dfe3f60326110d3bc7dc0eccb365f16431
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-01 10:33:56 -0800

    I fixed the problem of betty checker

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
-		return NULL;
+		return (NULL);
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
